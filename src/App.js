b7fdc8b1957cb7dc8ec0f78695c6292dad2efa9b
alexaldearroyo <alexaldearroyo@gmail.com>
2024-05-14 22:45:51 +0200

    Create Header component with styled-components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 // import './App.css';
 // import logo from './logo.svg';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: white;
+  color: black;
+  font-size: 2rem;
+  font-family: 'Arial', sans-serif;
+`;
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header>
         <p>React Random Color Generator</p>
-      </header>
+      </Header>
     </div>
   );
 }

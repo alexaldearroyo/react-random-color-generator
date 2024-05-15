471a6e03a5a5280944d72fdc5ace0a28692bff78
alexaldearroyo <alexaldearroyo@gmail.com>
2024-05-15 16:39:14 +0200

    Implement again styled-components module and add inline style modification for background color of div

--- src/App.js
+++ src/App.js
@@ -1,80 +1,93 @@
 import randomColor from 'randomcolor';
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
+// STYLES
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh;
+  color: black;
+  font-size: 1.5rem;
+  font-family: 'Arial', sans-serif;
+  vertical-align: middle;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh;
+  color: black;
+  font-size: 1rem;
+  font-family: 'Arial', sans-serif;
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  margin: 0 2rem;
+  border-radius: 10px;
+  background-color: ${(props) => props.bgColor};
+`;
+
+const Button = styled.button`
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 1rem;
+  color: black;
+  background-color: white;
+  border: 2px solid black;
+  border-radius: 5px;
+  cursor: pointer;
+  transition:
+    background-color 0.3s,
+    box-shadow 0.3s;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+
+  &:hover {
+    background-color: #f0f0f0;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);
+  }
+`;
+
+// APP COMPONENT
 export default function App() {
   const [bgColor, setBgColor] = useState(randomColor());
 
   const generateRandomColor = () => {
     const newColor = randomColor({ format: 'hex' });
-    console.log('Generated Color:', newColor); // Verifica que el color generado es válido
+    console.log('Generated Color:', newColor);
     setBgColor(newColor);
   };
 
   return (
-    <div
-      className="App"
-      style={{ textAlign: 'center', fontFamily: 'Arial, sans-serif' }}
-    >
-      <header
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '10vh',
-          color: 'black',
-          fontSize: '1.5rem',
-        }}
-      >
+    <div className="App">
+      <Header>
         <p>React Random Color Generator</p>
-      </header>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '80vh',
-          margin: '0 2rem',
-          borderRadius: '10px',
-          backgroundColor: bgColor,
-        }}
-      >
-        <div style={{ fontSize: '1.2rem', color: 'black' }}>
-          Generated Color: {bgColor}
-        </div>
-        <button
-          onClick={generateRandomColor}
+      </Header>
+      <Container bgColor={bgColor}>
+        <div
           style={{
-            marginTop: '20px',
-            padding: '10px 20px',
-            fontSize: '1rem',
+            fontSize: '1.2rem',
             color: 'black',
-            backgroundColor: 'white',
-            border: '2px solid black',
-            borderRadius: '5px',
-            cursor: 'pointer',
-            transition: 'background-color 0.3s, box-shadow 0.3s',
-            boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)',
+            backgroundColor: bgColor,
           }}
         >
-          Generate
-        </button>
-      </div>
-      <footer
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '10vh',
-          color: 'black',
-          fontSize: '1rem',
-          position: 'fixed',
-          width: '100%',
-          bottom: 0,
-        }}
-      >
+          Generated Color: {bgColor}
+        </div>
+        <Button onClick={generateRandomColor}>Generate</Button>
+      </Container>
+      <Footer>
         <p>Alex Arroyo © 2023</p>
-      </footer>
+      </Footer>
     </div>
   );
 }

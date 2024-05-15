385005576c810897e38d40bfc293375247a9ddd6
alexaldearroyo <alexaldearroyo@gmail.com>
2024-05-15 15:29:36 +0200

    Modify generateRandomColor function

--- src/App.js
+++ src/App.js
@@ -67,7 +67,9 @@ export default function App() {
   const [generatedColor, setGeneratedColor] = useState('');
 
   const generateRandomColor = () => {
-    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    const randomColor = `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, '0')}`;
     setBgColor(randomColor);
     setGeneratedColor(randomColor);
   };
